82500584c6467417b989c4d1030a24b4b54d2cd6
Jay Shah <jrshah01@gmail.com>
2020-07-04 15:21:33 +0100

    add entry point

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools.core import setup
+from setuptools import setup
 
 setup(
     name='Quorontine',
     version='0.1dev',
     packages=['quorontine',],
     long_description=open('README.txt').read(),
+    entry_points={
+        "console_scripts": [
+            "qorontine=qorontine.game:main",
+        ],
 )
