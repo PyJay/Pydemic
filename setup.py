09f391f79c6e40acee34630c6467d9c83dbce177
Jay Shah <jrshah01@gmail.com>
2020-07-04 15:40:50 +0100

    fix require

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1dev',
     packages=['qorontine',],
     long_description=open('README.md').read(),
+    install_requires=['arcade'],
     entry_points={
         "console_scripts": [
             "qorontine=qorontine.game:main",
