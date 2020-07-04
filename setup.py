dc218b2a722f13f6260da43692633a3bf39f7618
Jay Shah <jrshah01@gmail.com>
2020-07-04 15:24:00 +0100

    change readme.txt to .md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Quorontine',
     version='0.1dev',
     packages=['quorontine',],
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     entry_points={
         "console_scripts": [
             "qorontine=qorontine.game:main",
