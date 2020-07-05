e85e88f3426914e43369a899d0e678707f2de1c8
Jay Shah <jrshah01@gmail.com>
2020-07-05 15:38:05 +0100

    renaming

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Epydemic',
-    version='0.1.1',
-    packages=['epydemic',],
+    name='Pydemic',
+    version='0.1.2',
+    packages=['pydemic',],
     long_description=open('README.md').read(),
     install_requires=['arcade'],
     entry_points={
         "console_scripts": [
-            "epydemic=epydemic.game:main",
+            "pydemic=pydemic.game:main",
         ],},
-    package_data={'epydemic': ['data/*.png', 'data/*.mp3']}
+    package_data={'pydemic': ['data/*.png', 'data/*.mp3']}
 )
